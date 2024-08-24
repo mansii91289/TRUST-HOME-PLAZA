f60aa4710eb8d2802979d78651b4fe4b0010a17c
Mansi <mansi.1289@gmail.com>
2024-08-24 22:33:00 +0530

    functionality sign up

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -25,6 +25,7 @@ export default function Header() {
         <Link to='/about'>
         <li className='hidden sm:inline text-slate-700 hover:underline'>About</li>
         </Link>
+        
         <Link to='/sign-up'>
         <li className=' text-slate-700 hover:underline'>Sign in</li>
         </Link>
