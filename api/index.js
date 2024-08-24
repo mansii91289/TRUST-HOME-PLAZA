bc7a21347b7c3647da1641a376bffb03bb347e1e
Mansi <mansi.1289@gmail.com>
2024-08-24 12:25:07 +0530

    sign up api

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 dotenv.config();
 
 mongoose.connect(process.env.MONGODB).then(() => {
@@ -11,7 +13,12 @@ mongoose.connect(process.env.MONGODB).then(() => {
 
 const app = express();
 
+app.use(express.json());
+
 app.listen(3000, () =>{
   console.log('server is listening on port 3000');
 } 
 );
+
+app.use('/api/user', userRouter );
+app.use('/api/auth', authRouter);
